1b197d0696896fba7600d53beed286b7281f37bb
ChuqiaoHuang <chuqiao575@gmail.com>
2025-02-18 23:01:03 -0500

    import AboutUs

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,7 @@ import MessageStandalone from './MessageStandalone'
 import Home from './Home'
 import Header from './Header'
 import Footer from './Footer'
+import AboutUs from './AboutUs'
 
 const App = props => {
   return (
