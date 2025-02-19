c278dfae7fe6349fc90e4284a7c608c15a5fcf27
ChuqiaoHuang <chuqiao575@gmail.com>
2025-02-18 23:05:55 -0500

    biuld AboutUs.css

---
+++ front-end/src/AboutUs.js
@@ -0,0 +1,30 @@
+import './AboutUs.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+
+const AboutUs = () => {
+  const [name, setName] = useState("");
+  const [intro, setIntro] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_SERVER_HOSTNAME}/about`)
+      .then((res) => {
+        setName(res.data.name);
+        setIntro(res.data.info);
+        setImageUrl(res.data.photoURL);
+      })
+  })
+
+  return (
+    <>
+      <h1>About Us</h1>
+      <span>
+        <img src={imageUrl} />
+        <h2>{name}</h2>
+        <p>{intro}</p>
+      </span>
+    </>
+  )
+}
